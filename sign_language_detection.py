c0d4581800fe7b51b9dacb17a392010be6712e72
NoorBano Shaikh <167125592+noorbano1@users.noreply.github.com>
2025-11-15 10:38:10 +0500

    Update sign_language_detection.py

--- sign_language_detection.py
+++ sign_language_detection.py
@@ -169,6 +169,21 @@ plt.legend()
 plt.show()
 
 model.save('sign_language_cnn_model.h5')
+import pickle
+
+# 35 classes as per your trained model
+class_indices = {
+    'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9,
+    'K':10,'L':11,'M':12,'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,
+    'U':20,'V':21,'W':22,'X':23,'Y':24,'0':25,'1':26,'2':27,'3':28,'4':29,
+    '5':30,'6':31,'7':32,'8':33,'9':34
+}
+
+# Save labels.pkl
+with open("labels.pkl", "wb") as f:
+    pickle.dump(class_indices, f)
+
+print("labels.pkl created successfully for 35 classes!")
 
 from tensorflow.keras.preprocessing import image
 from google.colab import files
